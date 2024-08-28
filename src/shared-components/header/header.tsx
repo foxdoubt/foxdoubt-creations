6809e3add20134de9eb7f1536f0a15e922e03b8a
“Daniel <danielwdewald@gmail.com>
2024-08-28 17:27:59 -0400

    Add basic elements for logo (work still needed)

--- src/shared-components/header/header.tsx
+++ src/shared-components/header/header.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
+import img from "../../images/tavis-beck-fox-unsplash.jpg";
 
 const Header = () => {
-  const siteTitle = "Foxdoubt / creations";
+  const siteTitle = "foxdoubt / creations";
   const siteDescription = "By Daniel DeWald";
   return (
-    <div>
+    <div className="header-container">
       {/* <SiteLogo />
         <SiteTitle /> */}
+      <div className="site-logo-container">
+        <img className="site-logo" src={img} />
+      </div>
       <div className="site-title-container">
         <h1 className="font-site-title">{siteTitle}</h1>
         <p className="font-main">{siteDescription}</p>
