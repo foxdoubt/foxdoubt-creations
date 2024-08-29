047e12c91827685c4cbbcf5bd799d428601be331
“Daniel <danielwdewald@gmail.com>
2024-08-29 19:40:38 -0400

    Complete header

--- src/shared-components/header/header.tsx
+++ src/shared-components/header/header.tsx
@@ -4,20 +4,22 @@ const Header = () => {
   const siteTitle = ["foxdoubt", "/", "creations"];
   const siteDescription = "By Daniel DeWald";
   return (
-    <div className="header-container">
+    <div className="flex-row-center header-container">
       {/* <SiteLogo />
         <SiteTitle /> */}
-      <div className="site-logo-container">
-        <div className="site-logo"></div>
-      </div>
-      <div className="site-title-container">
-        <h1 className="font-site-title">
-          <span>{siteTitle[0]}</span>
-          <span className="accent-text">{siteTitle[1]}</span>
-          <span>{siteTitle[2]}</span>
-        </h1>
-        <p className="site-description">{siteDescription}</p>
-      </div>
+      <header className="flex-row-center">
+        <div className="flex-row-center site-logo-container">
+          <div className="site-logo"></div>
+        </div>
+        <div className="site-title-container">
+          <h1 className="font-site-title">
+            <span>{siteTitle[0]}</span>
+            <span className="accent-text">{siteTitle[1]}</span>
+            <span>{siteTitle[2]}</span>
+          </h1>
+          <p className="site-description">{siteDescription}</p>
+        </div>
+      </header>
     </div>
   );
 };
