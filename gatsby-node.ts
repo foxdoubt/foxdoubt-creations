b846c32681aedaa73ae4704e055325639947da05
“Daniel <danielwdewald@gmail.com>
2024-09-05 18:11:46 -0400

    Get first draft of post-to-post navigation working successfully

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -12,10 +12,9 @@ export const createPages: GatsbyNode["createPages"] = async ({
     PageProps<Queries.GetAllArtworkQuery>["data"]
   >(`
     query GetAllArtwork {
-      allSanityArtwork {
+      allSanityArtwork(sort: { publishedAt: ASC }) {
         edges {
           node {
-            title
             slug {
               current
             }
@@ -24,17 +23,29 @@ export const createPages: GatsbyNode["createPages"] = async ({
       }
     }
   `);
+
   if (errors) {
     throw errors;
   }
   const edges: ArtworkQueryEdges = data?.allSanityArtwork.edges || [];
 
-  edges.forEach(({ node }) => {
+  edges.forEach(({ node }, index) => {
     if (node.slug?.current) {
+      const prevIndex = index > 0 ? index - 1 : null;
+      const nextIndex = index < edges.length - 1 ? index + 1 : null;
+      const previousPost =
+        prevIndex !== null ? edges[prevIndex].node.slug?.current : null;
+      const nextPost =
+        nextIndex !== null ? edges[nextIndex].node.slug?.current : null;
+      const context = {
+        slug: { current: { eq: node.slug.current } },
+        previousPost,
+        nextPost,
+      };
       actions.createPage({
         path: `/artwork/${node.slug.current}`,
         component: path.resolve(`./src/templates/artwork/artwork.tsx`),
-        context: { slug: { current: { eq: node.slug.current } } },
+        context,
       });
     }
   });
--- src/templates/artwork/artwork.tsx
+++ src/templates/artwork/artwork.tsx
@@ -1,18 +1,44 @@
 import * as React from "react";
-import { PageProps, graphql } from "gatsby";
+import { PageProps, graphql, Link } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
-const Artwork = ({ data }: PageProps<Queries.GetArtworkPostQuery>) => {
+const Artwork = ({
+  data,
+  pageContext,
+}: PageProps<Queries.GetArtworkPostQuery>) => {
   const title = data.sanityArtwork?.title;
   const medium = data.sanityArtwork?.medium;
   const size = data.sanityArtwork?.size;
   const completionYear = data.sanityArtwork?.completionYear;
+  const gatsbyImageData = data.sanityArtwork?.mainImage?.asset?.gatsbyImageData;
   return (
     <div className="artwork-template-container">
       <div className="flex-column-center artwork-label">
         <h3 className="artwork-title">{title}</h3>
-        <p className="artwork-description">{[medium, size].join(", ")}</p>
-        <p className="artwork-year">{completionYear}</p>
+        <div className="navigation-container">
+          {(pageContext as any).previousPost ? (
+            <Link to={`/artwork/${(pageContext as any).previousPost}`}>
+              <p>prev</p>
+            </Link>
+          ) : (
+            <p>prev</p>
+          )}
+          <div className="flex-column-center">
+            <p className="artwork-description">{[medium, size].join(", ")}</p>
+            <p className="artwork-year">{completionYear}</p>
+          </div>
+          {(pageContext as any).nextPost ? (
+            <Link to={`/artwork/${(pageContext as any).nextPost}`}>
+              <p>next</p>
+            </Link>
+          ) : (
+            <p>next</p>
+          )}
+        </div>
       </div>
+      {gatsbyImageData && title && (
+        <GatsbyImage alt={title} image={gatsbyImageData} />
+      )}
     </div>
   );
 };
