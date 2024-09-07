54b9a39ff4c82c68f9464f909a130ff5d7d3e1d2
“Daniel <danielwdewald@gmail.com>
2024-09-07 12:59:13 -0400

    Replace 'prev' and 'next' buttons with left/right carets

--- src/templates/artwork/artwork.tsx
+++ src/templates/artwork/artwork.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { PageProps, graphql, Link } from "gatsby";
-import { GatsbyImage } from "gatsby-plugin-image";
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
 
 const Artwork = ({
   data,
@@ -11,29 +11,34 @@ const Artwork = ({
   const size = data.sanityArtwork?.size;
   const completionYear = data.sanityArtwork?.completionYear;
   const gatsbyImageData = data.sanityArtwork?.mainImage?.asset?.gatsbyImageData;
+  const currentPostPath = `/artwork/${(pageContext as any).slug.current.eq}`;
+  const prevPostPath = (pageContext as any).previousPost
+    ? `/artwork/${(pageContext as any).previousPost}`
+    : currentPostPath;
+
+  const nextPostPath = (pageContext as any).nextPost
+    ? `/artwork/${(pageContext as any).nextPost}`
+    : currentPostPath;
   return (
     <div className="artwork-template-container">
       <div className="flex-column-center artwork-label">
         <h3 className="artwork-title">{title}</h3>
         <div className="navigation-container">
-          {(pageContext as any).previousPost ? (
-            <Link to={`/artwork/${(pageContext as any).previousPost}`}>
-              <p>prev</p>
-            </Link>
-          ) : (
-            <p>prev</p>
-          )}
+          <Link to={prevPostPath}>
+            <StaticImage src={"../../images/caret-left.svg"} alt="left-arrow" />
+          </Link>
+
           <div className="flex-column-center">
             <p className="artwork-description">{[medium, size].join(", ")}</p>
             <p className="artwork-year">{completionYear}</p>
           </div>
-          {(pageContext as any).nextPost ? (
-            <Link to={`/artwork/${(pageContext as any).nextPost}`}>
-              <p>next</p>
-            </Link>
-          ) : (
-            <p>next</p>
-          )}
+
+          <Link to={nextPostPath}>
+            <StaticImage
+              src={"../../images/caret-right.svg"}
+              alt="right-arrow"
+            />
+          </Link>
         </div>
       </div>
       {gatsbyImageData && title && (
