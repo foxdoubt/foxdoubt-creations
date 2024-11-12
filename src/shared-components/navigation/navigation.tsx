798b3f06dd5b9532a92853a7ce03ce3fe0a9b1cb
“Daniel <danielwdewald@gmail.com>
2024-11-12 16:14:38 -0500

    Disable categories w/ no associated posts

--- src/shared-components/navigation/navigation.tsx
+++ src/shared-components/navigation/navigation.tsx
@@ -2,7 +2,11 @@ import * as React from "react";
 
 import { Link, graphql, useStaticQuery } from "gatsby";
 
-const Navigation = ({ selectedNavItem }: { selectedNavItem: string }) => {
+const Navigation = ({
+  selectedNavItem,
+}: {
+  selectedNavItem: string | null;
+}) => {
   const result = useStaticQuery<Queries.getAllCategoriesQuery>(graphql` 
     query getAllCategories {
       allSanityCategory(sort:{ title:ASC }) {
@@ -25,13 +29,23 @@ const Navigation = ({ selectedNavItem }: { selectedNavItem: string }) => {
           <div className="nav-item-container">
             {result.allSanityCategory.edges.map(({ node }, i) => {
               const slug = node.slug?.current;
-              const isCurrentNavItem = slug === selectedNavItem;
-              const classNames = isCurrentNavItem
-                ? "selected nav-item"
-                : "nav-item";
+
+              // temporary link disabling for non-existent category pages
+              let classNames = "nav-item disabled";
+              let linkPath = "#";
+
+              if (slug === "artwork") {
+                classNames = "nav-item";
+                linkPath = `/${slug}`;
+              }
+
+              classNames =
+                selectedNavItem && selectedNavItem === slug
+                  ? classNames.concat(" ", "selected")
+                  : classNames;
 
               return (
-                <Link to={`/${slug}`}>
+                <Link to={linkPath}>
                   <p className={classNames} key={`nav-item-${node.title}-${i}`}>
                     {node.title}
                   </p>
