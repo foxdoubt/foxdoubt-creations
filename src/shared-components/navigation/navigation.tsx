226a0bc30d226c510c759cf13e32d066ae09dc66
“Daniel <danielwdewald@gmail.com>
2024-11-12 15:45:20 -0500

    Dynamically display categories

--- src/shared-components/navigation/navigation.tsx
+++ src/shared-components/navigation/navigation.tsx
@@ -1,22 +1,41 @@
 import * as React from "react";
 
-const Navigation = () => {
-  const props = ["about", "artwork", "web dev", "musings"];
-  const [aboutNav, ...items] = props;
-  const selected = "artwork";
+import { Link, graphql, useStaticQuery } from "gatsby";
+
+const Navigation = ({ selectedNavItem }: { selectedNavItem: string }) => {
+  const result = useStaticQuery<Queries.getAllCategoriesQuery>(graphql` 
+    query getAllCategories {
+      allSanityCategory(sort:{ title:ASC }) {
+        edges {
+          node {
+            title
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }`);
+
   return (
     <div className="site-navigation-container">
       <div className="nav-center">
         <nav className="site-navigation">
-          <p className="nav-item about-nav-item">{aboutNav}</p>
+          <p className="nav-item about-nav-item">about</p>
           <div className="nav-item-container">
-            {items.map((navItem, i) => {
-              const isSelected = navItem === selected;
-              const classNames = isSelected ? "selected nav-item" : "nav-item";
+            {result.allSanityCategory.edges.map(({ node }, i) => {
+              const slug = node.slug?.current;
+              const isCurrentNavItem = slug === selectedNavItem;
+              const classNames = isCurrentNavItem
+                ? "selected nav-item"
+                : "nav-item";
+
               return (
-                <p className={classNames} key={`nav-item-${navItem}-${i}`}>
-                  {navItem}
-                </p>
+                <Link to={`/${slug}`}>
+                  <p className={classNames} key={`nav-item-${node.title}-${i}`}>
+                    {node.title}
+                  </p>
+                </Link>
               );
             })}
           </div>
