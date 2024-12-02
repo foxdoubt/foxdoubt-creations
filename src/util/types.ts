e5bca2a9f06c70a415243248f94397d51299e9e7
“Daniel <danielwdewald@gmail.com>
2024-12-02 12:31:39 -0500

    Add previously deleted type

--- src/util/types.ts
+++ src/util/types.ts
@@ -2,3 +2,8 @@ export type SelectedWorks = Queries.SanityShow["selectedWorks"];
 export type Artwork = {
   readonly slug: { readonly current: string | null } | null;
 };
+
+export interface IPostLinkState {
+  nextStepLinkText: string;
+  nextStepsLinkPath: string;
+}
